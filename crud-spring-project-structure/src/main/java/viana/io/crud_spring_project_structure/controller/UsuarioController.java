5b8936da62a1f22f80ad233274e625fb78a24ac8
Leonardo Viana <leonardoviana00@hotmail.com>
2025-07-28 10:39:24 -0300

    feat(controller): creating the controller layer

---
+++ crud-spring-project-structure/src/main/java/viana/io/crud_spring_project_structure/controller/UsuarioController.java
@@ -0,0 +1,46 @@
+package viana.io.crud_spring_project_structure.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import viana.io.crud_spring_project_structure.business.UsuarioService;
+import viana.io.crud_spring_project_structure.infrastructure.entitys.Usuario;
+
+@RestController
+@RequestMapping("/usuario")
+@RequiredArgsConstructor
+public class UsuarioController {
+
+    private final UsuarioService usuarioService;
+
+    @PostMapping
+    public ResponseEntity<Void> salvarUsuario(@RequestBody Usuario usuario){
+        usuarioService.salvarUsuario(usuario);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<Usuario> buscarUsuarioPorEmail(@RequestParam String email){
+        return ResponseEntity.ok(usuarioService.buscUsuarioPorEmail(email));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deletarUsuarioPorEmail(@RequestParam String email){
+        usuarioService.deletarUsuarioPorEmail(email);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping
+    public ResponseEntity<Void> atualizarUsuarioPorId(@RequestParam Integer id, @RequestBody Usuario usuario){
+        usuarioService.atualizarUsuarioPorId(id, usuario);
+        return ResponseEntity.ok().build();
+    }
+}
